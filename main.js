56b48c04c9e74b3f537d82504168208f1d3717c1
isabelle-xiong <yeelamisabelle.xiong@duke.edu>
2024-02-04 03:39:32 -0500

    made all buttons functional. Next step: link projects and make contact form usable

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+/*==========toggle icon navbar===========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    // when you click, icon will become an x
+    menuIcon.classList.toggle('bx-x');
+    // Toggles presence of class active in navbar. If the active class is not present, it will be added; if it is already present, it will be removed.
+    navbar.classList.toggle('active');
+};
+
 /*==========scroll sections active link===========*/
 // Select all <section> elements on the page
 let sections = document.querySelectorAll('section');
@@ -27,7 +38,46 @@ window.onscroll = () => {
         };
     });
     /*==========sticky navbar===========*/
+    // Select the <header> element in the document
     let header = document.querySelector('header')
-
+    // Accesses the classes of the <header> element,toggles the presence of the class 'sticky' on the <header> element based on the condition window.scrollY > 100
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==========remove toggle icon and navbar when click navbar link (scroll)===========*/
+
+    // when you scroll it closes menu
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active');
 };
+
+/*==========scroll reveal===========*/
+
+/* distance: '80px': This option sets the distance from the viewport edge at which the elements start to reveal. In this case, elements will start to reveal when they are within 80 pixels from the edge of the viewport.
+
+duration: 2000: This option sets the duration of the reveal animation in milliseconds. In this case, the animation duration is set to 2000 milliseconds (or 2 seconds).
+
+delay: 200: This option sets the delay before the reveal animation starts in milliseconds. In this case, there is a delay of 200 milliseconds before the reveal animation begins.
+
+reset: true (commented out): This option, when enabled, resets the reveal configuration for elements each time they become visible. This means that if an element becomes hidden and then visible again (for example, when scrolling back up the page), the reveal animation will happen again. By default, reset is set to false.*/
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+// when you first go to page, animation effects for stuff on page, can get this line in scroll reveal page, just copy paste
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*==========typed js===========*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Fullstack Developer', 'iOS Developer', 'Canadian National Team Athlete', 'Entrepreneur', 'Problem Solver', 'Avid Learner'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
